6e305316aabd225f6d2e1d005100566bb964c8ed
Suryandini <suryandini04@gmail.com>
2022-10-18 14:08:46 +0700

    project kedua

---
+++ operator penugasan.py
@@ -0,0 +1,20 @@
+#operator penugasan
+nilai1 = 5
+nilai0 = 42
+nilai1 += 6 #(nilai +=5) sama dengan (nilai = nilai+5)
+nilai0 =+ 6 # nilai0 =+ 0 sama dengan nilai0 = 6
+print (nilai1)
+print (nilai0)
+nilai1 -= 5
+print (nilai1)
+nilai1 *= 7
+print(nilai1)
+nilai1 /= 3
+print (nilai1)
+nilai1 %= 6
+print (nilai1)
+nilai1 **= 2
+print (nilai1)
+nilai1 //=5
+print (nilai1)
+
